7c54941606ab5a409d8a504f83fe75527d493c28
tarangokhan <tarangokhan77@gmailcom>
2020-04-22 18:35:43 +0600

    Add sign up and login page

--- routes/home.js
+++ routes/home.js
@@ -7,4 +7,12 @@ router.showHome = function(req, res, next) {
     res.render('index', { title: 'This Is The Homepage' , pageTitle: 'Home'});
 }
 
+router.showLogIn = function(req, res, next) {
+    res.render('login', {});
+}
+
+router.showSignUp = function(req, res, next) {
+    res.render('signup', {});
+}
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var homeController = require('./home.js');
 module.exports = function(newApp) {
     /* GET home page. */
     newApp.get('/', homeController.showHome);
+    newApp.get('/login/', homeController.showLogIn);
+    newApp.get('/signup/', homeController.showSignUp);
 }
-
