cc5913b415cd833d3906a5f0b884616f6aad037e
tarangokhan <tarangokhan77@gmailcom>
2020-04-24 21:37:06 +0600

    Add category suggestion form

--- routes/category.js
+++ routes/category.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 
-router.addProblemForm = function(req, res, next) {
-    res.render('add_problem', {});
+router.addCategoryForm = function(req, res, next) {
+    res.render('add_category', {});
 }
-router.problemSubmit = function(req, res, next) {
+router.categorySubmit = function(req, res, next) {
     // Submit data to server (req.data)
     res.redirect('/');
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 
 var homeController = require('./home.js');
 var problemController = require('./problem.js');
+var categoryController = require('./category.js');
 
 module.exports = function(newApp) {
     /* GET home page. */
     newApp.get('/', homeController.showHome);
     newApp.get('/login/', homeController.showLogIn);
     newApp.get('/signup/', homeController.showSignUp);
+
     newApp.get('/problem/add/', problemController.addProblemForm);
+    newApp.post('/problem/submit/', problemController.problemSubmit);
+
+    newApp.get('/category/add/', categoryController.addCategoryForm);
+    newApp.post('/category/submit/', categoryController.categorySubmit);
 }
