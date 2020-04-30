7cbfb235f58d997a621b9366d60e81c460772321
Tarango Khan <tarango.khan@tigerit.com>
2020-04-30 19:15:26 +0600

    Add team member comparison chart

--- routes/team.js
+++ routes/team.js
@@ -5,6 +5,7 @@ var config = require('../config');
 var router = express.Router();
 const axios = require('axios');
 var team_search_url = config.server_host + 'team/search/'
+var user_search_url = config.server_host + 'user/search/'
 var team_submit_url = config.server_host + 'team/'
 
 
@@ -22,6 +23,23 @@ async function getTeamList(search_param) {
   }
   console.log('getTeamList done');
   return team_list
+}
+
+
+async function getUserList(search_param) {
+  console.log('getUserList called');
+  var page = 0
+  var user_list = []
+  while(1){
+    var post_url = user_search_url + page.toString()
+    console.log("post_url: " + post_url)
+    let res = await axios.post(post_url, search_param);
+    user_list = res.data
+    page++
+    break;
+  }
+  console.log('getUserList done');
+  return user_list
 }
 
 async function postTeam(team_data) {
@@ -44,8 +62,10 @@ async function teamDetails(team_id, req) {
   return res.data
 }
 
-router.addTeamForm = function(req, res, next) {
-  res.render('add_team', {});
+router.addTeamForm = async function(req, res, next) {
+  let user_list = await getUserList({});
+  console.log(user_list)
+  res.render('add_team', user_list);
 }
 
 function update_team_data(team){
@@ -77,8 +97,10 @@ function update_team_data(team){
 }
 
 router.addTeamFormSubmit = async function(req, res, next) {
+  console.log('addTeamFormSubmit called')
   var team = req.body
   team['team_type'] = 'team';
+  console.log(team)
   team = update_team_data(team)
   await postTeam(team)
   let team_list = await getTeamList({});
--- routes/training.js
+++ routes/training.js
@@ -5,12 +5,20 @@ var config = require('../config');
 var router = express.Router();
 const axios = require('axios');
 var individual_training_url = config.server_host + 'training/individual'
+var team_training_url = config.server_host + 'training/team/'
 
 async function getIndividualTrainingModel() {
     console.log('getIndividualTrainingModel called');
     console.log("post_url: " + individual_training_url)
     let res = await axios.get(individual_training_url);
     console.log('getIndividualTrainingModel done');
+    return res.data
+}
+
+async function getTeamTrainingModel(team_id) {
+    var url = team_training_url + team_id
+    let res = await axios.get(url);
+    console.log('getTeamTrainingModel done');
     return res.data
 }
 
@@ -23,7 +31,6 @@ router.viewIndividualTraining = async function(req, res, next) {
     len = training_data['problem_stat'].length
     for(idx = 1;idx<=len;idx++){
         training_data['problem_stat'][idx-1]['idx'] = idx;
-        console.log(training_data['problem_stat'][idx-1])
     }
 
     len = training_data['category_stat'].length
@@ -37,6 +44,33 @@ router.viewIndividualTraining = async function(req, res, next) {
     }
 
     res.render('individual_training', training_data);
+}
+
+
+router.viewTeamTraining = async function(req, res, next) {
+    var team_id = "pxfAynEBDdZ5mbgHtL56"
+    var training_data = await getTeamTrainingModel(team_id)
+
+    console.log(training_data)
+
+    var len, idx;
+
+    len = training_data['problem_stat'].length
+    for(idx = 1;idx<=len;idx++){
+        training_data['problem_stat'][idx-1]['idx'] = idx;
+    }
+
+    len = training_data['category_stat'].length
+    for(idx = 1;idx<=len;idx++){
+        training_data['category_stat'][idx-1]['idx'] = idx;
+    }
+
+    len = training_data['category_skill_list'].length
+    for(idx = 1;idx<=len;idx++){
+        training_data['category_skill_list'][idx-1]['idx'] = idx;
+    }
+
+    res.render('team_training', training_data);
 }
 
 module.exports = router;
